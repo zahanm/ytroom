c278d0b4c7fe1b6ac22a78cfe45aecd28ff4e16e
Zahan Malkani <zahanm@gmail.com>
2011-08-02 00:12:16 -0700

    Working yt player
    using swfobject to embed

--- src/app.js
+++ src/app.js
@@ -7,14 +7,13 @@ var dom = require('../lib/uki-core/dom'),
 dom.createStylesheet(__requiredCss);
 
 window.startApp = function() {
-  FB.api(
-      '/me/friends',
-      { fields: ['first_name', 'name', 'id'], limit: 30 },
-      function(friends) {
-        var lines = friends['data'].map(function(f) {
-                return f.name + " | id: " + f.id;
-              });
-        document.getElementById('starter').innerHTML = lines.join('<br/>');
-      }
-  );
+  var params = { allowScriptAccess: "always" };
+  var atts = { id: "ytplayer" };
+  swfobject.embedSWF("http://www.youtube.com/e/0NKUpo_xKyQ?enablejsapi=1&playerapiid=ytplayer",
+    "ytplayer", "425", "356", "8", null, null, params, atts);
+};
+
+window.onYouTubePlayerReady = function() {
+  var player = document.getElementById('ytplayer');
+  player && player.playVideo();
 };
